c8834d7aa3278065c6aeac7b6d9365e1428509cc
Andrei Smirnov <andrei.smirnov.69@gmail.com>
2020-07-27 11:49:02 -0400

    chrom js dont insert space after colon in json

--- src/libdipole/proto.cpp
+++ src/libdipole/proto.cpp
@@ -16,7 +16,7 @@ string Dipole::create_new_message_id()
 
 Dipole::message_type_t Dipole::get_message_type(const string& msg)
 {  
-  static const regex message_type_re(R"D("message-type": "([\w-]+)")D");
+  static const regex message_type_re(R"D("message-type": ?"([\w-]+)")D");
   smatch ma;
   if (regex_search(msg, ma, message_type_re)) {
     if (ma.ready() && ma.size() == 2) {
@@ -34,7 +34,7 @@ Dipole::message_type_t Dipole::get_message_type(const string& msg)
 
 string Dipole::get_method_signature(const string& msg)
 {
-  static const regex method_signature_re(R"D("method-signature": "([\w:]+)")D");
+  static const regex method_signature_re(R"D("method-signature": ?"([\w:]+)")D");
   smatch ma;
   if (regex_search(msg, ma, method_signature_re)) {
     if (ma.ready() && ma.size() == 2) {
@@ -50,7 +50,7 @@ string Dipole::get_method_signature(const string& msg)
 
 string Dipole::get_orig_message_id(const string& msg)
 {
-  static const regex orig_message_id_re(R"D("orig-message-id": "([\w-]+)")D");
+  static const regex orig_message_id_re(R"D("orig-message-id": ?"([\w-]+)")D");
   smatch ma;
   if (regex_search(msg, ma, orig_message_id_re)) {
     if (ma.ready() && ma.size() == 2) {
