da2bf4b82cabc301c2a8d51cd1d72e86848de656
Andrei Smirnov <andrei.smirnov.69@gmail.com>
2020-07-30 13:54:12 -0400

    dipole server connected

--- examples/blotter/frontend/App.js
+++ examples/blotter/frontend/App.js
@@ -1,53 +1,57 @@
-import React from 'react';
-import { Grid, Table, TableHeaderRow } from '@devexpress/dx-react-grid-bootstrap4';
+import React, { useState } from 'react';
+import { Grid,
+	 Table,
+	 TableHeaderRow,
+	 DragDropProvider,
+	 TableColumnReordering } from '@devexpress/dx-react-grid-bootstrap4';
 import '@devexpress/dx-react-grid-bootstrap4/dist/dx-react-grid-bootstrap4.css';
 
 import * as libdipole from 'libdipole-js';
 import * as Blotter from './Blotter.js';
 
-const columns = [{ name: 'id', title: 'ID' },
-		 { name: 'product', title: 'Product' },
-		 { name: 'owner', title: 'Owner' }];
-const rows = [{ id: 0, product: 'DevExtreme', owner: 'DevExpress' },
-	      { id: 1, product: 'DevExtreme Reactive', owner: 'DevExpress' }
-	     ];
+function App() {
+    let comm = new libdipole.Communicator();
+    const [columns, setColumns] = useState([{ name: 'id', title: 'ID' },
+				{ name: 'product', title: 'Product' },
+				{ name: 'owner', title: 'Owner' }]);
+    const [rows] = useState([{ id: 0, product: 'DevExtreme', owner: 'DevExpress' },
+			     { id: 1, product: 'DevExtreme Reactive', owner: 'DevExpress' }
+			    ]);
 
-class App extends React.Component {
-    constructor(props) {
-	super(props);
-	this.comm = new libdipole.Communicator();
-	this.state = {blotter: null}
-	this.onClick = this.onClick.bind(this);
-    }
+    const [columnOrder, setColumnOrder] = useState(['id', 'product', 'owner']);
 
-    componentDidMount() {
+    let blotter_ptr;
+    React.useEffect(() => {
 	let ws_url = "ws://localhost:8080/";
-	let object_id = "test_df";
-	this.comm.connect(ws_url, object_id).then(o_ptr => {
-	    this.blotter_ptr = new Blotter.DFTestPtr(o_ptr);
-	    return this.blotter_ptr.get_df();
+	let object_id = "test_df";	
+	comm.connect(ws_url, object_id).then(o_ptr => {
+	    blotter_ptr = new Blotter.DFTestPtr(o_ptr);
+	    return blotter_ptr.get_df();
 	}).then(df_json => {
 	    console.log("server response:", df_json);
-	    this.setState({...this.state, blotter: df_json.retval.dataframeJSON});
+	    //this.setState({...this.state, blotter: df_json.retval.dataframeJSON});
+	});
+    });
+
+    const onClick = () => {
+	blotter_ptr.get_df().then(df_json => {
+	    console.log("onClick:", df_json);
 	});
     }
     
-    onClick() {
-    }
-    
-    render() {
-	return (<div>
-		<h1>{this.state.blotter}</h1>
-		<button ref={r => this.pb = r} onClick={this.onClick}>PRESS</button>
-		<div className="card">
-		<Grid rows={rows} columns={columns}>
-		<Table />
-		<TableHeaderRow />
-		</Grid>
-		</div>
-		
-	       </div>);
-    }
-};
+    return (<div>
+	    <div className="card">
+	    <Grid rows={rows} columns={columns}>
+	    <DragDropProvider />
+	    <Table />
+	    <TableColumnReordering order={columnOrder}
+	    onOrderChange={setColumnOrder}
+	    />
+	    <TableHeaderRow />
+	    </Grid>
+	    </div>
+	    <button onClick={onClick}>PRESS</button>
+	    </div>);
+}
 
 export default App;
